8400b5d2d5e041ace8c80ab3c9216763df8877f2
Kulbir Singh Gill <763495@student.tor.loc>
2020-02-13 11:57:49 -0500

    work

---
+++ myjava.js
@@ -0,0 +1,15 @@
+document.write("happy vaentine day to everyone")
+var1=5,var2=10;
+document.write(var1+var2);
+function b1()
+{
+    alert("welcome to page")
+}
+function sqare(number)
+{
+  return  number*number;
+}
+function apple(){
+    while(i<=100)
+     document.write('<tr><th>' + i + '</th></tr>')
+}
